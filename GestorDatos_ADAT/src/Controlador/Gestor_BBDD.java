82465513b77fdf43e0bb36e02704c0c1403b36f0
davidgimenez1997 <davidgimenez1997clase@gmail.com>
2018-10-23 20:23:38 +0200

    Leer bbdd hashmap

--- GestorDatos_ADAT/src/Controlador/Gestor_BBDD.java
+++ GestorDatos_ADAT/src/Controlador/Gestor_BBDD.java
@@ -5,6 +5,9 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Properties;
 
@@ -39,14 +42,62 @@ public class Gestor_BBDD implements Interfaz_Controlador{
 
 	@Override
 	public HashMap<String, Actor> leertodosActores() throws IOException {
-		// TODO Auto-generated method stub
-		return null;
+		HashMap<String,Actor> actoresCreados = new HashMap<>();
+		PreparedStatement pstm;
+		Actor act;
+		try {
+			pstm = conexion.prepareStatement("SELECT * FROM actores");
+			ResultSet rset = pstm.executeQuery();
+			while (rset.next()) {
+				act = new Actor();
+				act.setId(rset.getString("Id"));
+				act.setNombre(rset.getString("Nombre"));
+				act.setDescripcion(rset.getString("Descripcion"));
+				act.setPelo(rset.getString("Pelo"));
+				act.setOjos(rset.getString("Ojos"));
+				act.setRepresentante(crearRepresentante(rset.getString("Representante")));
+				actoresCreados.put(rset.getString("Id"), act);
+			}
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+		}
+		return actoresCreados;
 	}
+	
+	 public Representante crearRepresentante(String id_repre) {
+	        Representante repre = new Representante();
+	        try (PreparedStatement stmt = conexion.prepareStatement("SELECT * FROM representantes WHERE Id = " + id_repre)) {
+	            ResultSet rs = stmt.executeQuery();
+	            while (rs.next()) {
+	                repre = new Representante(rs.getString("Id"),rs.getString("Nombre"),rs.getString("Edad"));
+	            }
+	        } catch (SQLException e) {
+	            e.printStackTrace();
+	        }
+	        return repre;
+	    }
 
 	@Override
 	public HashMap<String, Representante> leertodosRepresentante() throws IOException {
-		// TODO Auto-generated method stub
-		return null;
+		HashMap<String,Representante> representantesCreados = new HashMap<>();
+		PreparedStatement pstm;
+		Representante repre;
+		try {
+			pstm = conexion.prepareStatement("SELECT * FROM representantes");
+			ResultSet rset = pstm.executeQuery();
+			while (rset.next()) {
+				repre = new Representante();
+				repre.setId(rset.getString("Id"));
+				repre.setNombre(rset.getString("Nombre"));
+				repre.setEdad(rset.getString("Edad"));
+				representantesCreados.put(rset.getString("Id"),repre);
+			}
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+		}
+		return representantesCreados;
 	}
 
 	@Override
