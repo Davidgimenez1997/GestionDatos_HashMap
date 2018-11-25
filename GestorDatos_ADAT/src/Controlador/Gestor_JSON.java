a641e5cda33a854764671db0f43caa2acee4c88f
davidgimenez1997 <davidgimenez1997clase@gmail.com>
2018-11-25 04:29:45 +0100

    Listado y insert actor

--- GestorDatos_ADAT/src/Controlador/Gestor_JSON.java
+++ GestorDatos_ADAT/src/Controlador/Gestor_JSON.java
@@ -3,8 +3,6 @@ package Controlador;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Properties;
 
@@ -59,7 +57,7 @@ public class Gestor_JSON implements I_GestorDatos {
 					String pelo;
 					String ojos;
 					String str_representante;
-					Representante representante = new Representante();
+					Representante representante;
 
 					for (int i = 0; i < actoresarr.size(); i++) {
 						JSONObject row = (JSONObject) actoresarr.get(i);
@@ -71,7 +69,8 @@ public class Gestor_JSON implements I_GestorDatos {
 						ojos = row.get("ojos").toString();
 						// str_representante =
 						// row.get("representante").toString();
-						// representante = row.get("representante");
+						str_representante = row.get("representante").toString();
+						representante = new Representante(str_representante);
 
 						actor = new Actor(id, nombre, descripcion, pelo, ojos, (Representante) representante);
 
@@ -151,7 +150,74 @@ public class Gestor_JSON implements I_GestorDatos {
 
 	@Override
 	public boolean agregarActor(Actor nuevo) throws IOException {
+		if (!comprobaridactor(nuevo)) {
+			System.out.println("El id del actor esta repetido");
+		} else {
+			try {
+				JSONObject objActor = new JSONObject();
+				JSONObject objPeticion = new JSONObject();
 
+				objActor.put("id", nuevo.getId());
+				objActor.put("nombre", nuevo.getNombre());
+				objActor.put("descripcion", nuevo.getDescripcion());
+				objActor.put("pelo", nuevo.getPelo());
+				objActor.put("ojos", nuevo.getOjos());
+				objActor.put("representante", nuevo.getRepresentante().getId());
+
+				objPeticion.put("actorAnnadir", objActor);
+				objPeticion.put("peticion", "add");
+
+				String json = objPeticion.toJSONString();
+
+				System.out.println("Lanzamos peticion JSON para almacenar un equipo");
+
+				String url = SERVER_PATH + SET_ACTOR;
+
+				String response = encargadoPeticiones.postRequest(url, json);
+
+				System.out.println("El json que recibimos es: ");
+
+				System.out.println(response); // Traza para pruebas
+				// System.exit(-1);
+
+				// Parseamos la respuesta y la convertimos en un JSONObject
+
+				JSONObject respuesta = (JSONObject) JSONValue.parse(response.toString());
+
+				if (respuesta == null) { // Si hay algún error de parseo (json
+											// incorrecto porque hay algún
+											// caracter
+											// raro, etc.) la respuesta será
+											// null
+					System.out.println("El json recibido no es correcto. Finaliza la ejecución");
+					System.exit(-1);
+				} else { // El JSON recibido es correcto
+
+					// Sera "ok" si todo ha ido bien o "error" si hay algún
+					// problema
+					String estado = (String) respuesta.get("estado");
+					if (estado.equals("ok")) {
+
+						System.out.println("Almacenado equipo enviado por JSON Remoto");
+						return true;
+
+					} else { // Hemos recibido el json pero en el estado se nos
+								// indica que ha habido algún error
+
+						System.out.println("Acceso JSON REMOTO - Error al almacenar los datos");
+						System.out.println("Error: " + (String) respuesta.get("error"));
+						System.out.println("Consulta: " + (String) respuesta.get("query"));
+
+					}
+				}
+			} catch (Exception e) {
+				System.out.println(
+						"Excepcion desconocida. Traza de error comentada en el método 'annadirEquipo' de la clase JSON REMOTO");
+				// e.printStackTrace();
+				System.out.println("Fin ejecución");
+				System.exit(-1);
+			}
+		}
 		return false;
 	}
 
@@ -228,7 +294,13 @@ public class Gestor_JSON implements I_GestorDatos {
 
 	@Override
 	public boolean comprobaridactor(Actor nuevo) throws IOException {
-		// TODO Auto-generated method stub
+		for (HashMap.Entry<String, Actor> entry : leertodosActores().entrySet()) {
+			if (entry.getKey().equals(nuevo.getId())) {
+				return false;
+			} else {
+				return true;
+			}
+		}
 		return false;
 	}
 
@@ -247,8 +319,10 @@ public class Gestor_JSON implements I_GestorDatos {
 
 	@Override
 	public void escribirtodosActores(HashMap<String, Actor> lista) throws IOException {
-		// TODO Auto-generated method stub
-
+		borrarTodoActores();
+		for (HashMap.Entry<String, Actor> entry : lista.entrySet()) {
+			agregarActor(lista.get(entry.getKey()));
+		}
 	}
 
 	@Override
